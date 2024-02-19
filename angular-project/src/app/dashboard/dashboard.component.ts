ec01eec31019ff4f10ce452290b9b2ef8912d24f
Martin Popper <martin.popper@siemens-healtineers.com>
2024-02-19 13:39:34 +0100

    Use takeUntilDestroyed instead of manual unsubscribe

--- angular-project/src/app/dashboard/dashboard.component.ts
+++ angular-project/src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,9 @@
-import { Component, inject, OnDestroy, OnInit } from '@angular/core';
-import { Observable, Subject, takeUntil, tap } from 'rxjs';
+import { Component, inject, OnInit } from '@angular/core';
 import { TestService } from '../test.service';
 import { AsyncPipe, NgForOf, NgIf } from '@angular/common';
 import { MatList, MatListItem } from '@angular/material/list';
 import { MatCell, MatCellDef, MatColumnDef, MatHeaderCell, MatHeaderCellDef, MatHeaderRow, MatHeaderRowDef, MatRow, MatRowDef, MatTable, MatTableDataSource } from '@angular/material/table';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-dashboard',
@@ -28,20 +28,14 @@ import { MatCell, MatCellDef, MatColumnDef, MatHeaderCell, MatHeaderCellDef, Mat
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent implements OnInit, OnDestroy {
+export class DashboardComponent implements OnInit {
   private testService = inject(TestService);
 
-  private unsubscribe$ = new Subject<void>();
   dataSource: MatTableDataSource<string>;
 
   ngOnInit(): void {
     this.testService.getNames()
-      .pipe(takeUntil(this.unsubscribe$))
+      .pipe(takeUntilDestroyed())
       .subscribe(names => this.dataSource = new MatTableDataSource(names));
-  }
-
-  ngOnDestroy(): void {
-    this.unsubscribe$.next();
-    this.unsubscribe$.complete();
   }
 }

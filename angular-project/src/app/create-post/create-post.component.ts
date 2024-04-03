9482938314b6c254dad99c33e2b2f09b7bdc3bad
zasik <zaso.juris@gmail.com>
2024-04-03 20:24:24 +0200

    Finalization of Desing and some TS
    Finished design of Reactive Forms
    Create Post values from reactive form displays in console
    
    Co-Authored-By: samuelmalik <113421307+samuelmalik@users.noreply.github.com>

--- angular-project/src/app/create-post/create-post.component.ts
+++ angular-project/src/app/create-post/create-post.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-create-post',
   standalone: true,
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatCardModule,
   ],
   templateUrl: './create-post.component.html',
   styleUrl: './create-post.component.css'
@@ -15,10 +17,13 @@ export class CreatePostComponent {
 
   constructor(private formBuilder: FormBuilder) {
     this.CreatePostForm = this.formBuilder.group({
-      name: ['', Validators.required, Validators.maxLength(50)],
+      title: ['', Validators.required, Validators.maxLength(50)],
       description: ['', Validators.required, Validators.maxLength(500)],
       code : ['']
     });
 
+  }
+  submitPost() {
+    console.log(this.CreatePostForm.value);
   }
 }
